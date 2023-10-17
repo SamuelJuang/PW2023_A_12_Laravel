135d479995150ec0962e24945bb7f466d33254f6
SamuelJuang <samueljuang007@gmail.com>
2023-10-17 22:13:47 +0700

    changedNavbartoAnotherPage

--- resources/views/frontPage.blade.php
+++ resources/views/frontPage.blade.php
@@ -1,74 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>SAF Kereta Api Online</title>
-    <link rel="stylesheet" href="{{ asset('css/animations.css') }}">
-    <style>
-        body{
-            overflow: hidden;
-            background-image: "{{asset('images/tempTrainStation.jpg')}}";
-            height: 100vh;
-            background-size: cover;
-            background-image: url('https://images.unsplash.com/photo-1495313196544-7d1adf4e628f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2071&q=80');
-            background-color: rgba(0, 0, 0, 0.5);
-            background-blend-mode: darken;
-            background-repeat: no-repeats;
-        }
-        hr {
-            border: 0;
-            clear:both;
-            display:block;
-            width: 96%;               
-            background-color:#ffffff;
-            height: 3px;
-            opacity: 100%;
-        }
-        footer {
-            font-size: small;
-            position:fixed;
-            left:0px;
-            bottom:0px;
-            height:30px;
-            width:100%;
-        }
-    </style>
-</head>
-<body>
-    <div class="mainInfo px-5">
-        <nav class="navbar py-3 px-3 topdownAnimated" aria-label="First navbar example">    
-            <img src="{{asset('images/SAF Logo.svg')}}" alt="" class="navbar-brand" style="height: 7vh;">
-          <div class="d-flex justify-content-end px-3 py-2">
-              <button class="btn btn-outline-light px-5 mx-3">
-                  login
-              </button>
-              <button class="btn btn-primary  px-5">
-                  register
-              </button>
-          </div>
-        </nav>
+@extends('PageNavFoot')
 
-        <div class="d-flex align-content-center fadeInAnimated">
-            <div class="mx-auto mt-4 text-white text-center">
-                <h1><strong>Merajut Memori Bersama Kami.</strong></h1>
-                <h5>Dengan fasilitas dan pelayanan yang anda dambakan</h5>
+@section('content')
+
+    <div class="d-flex align-content-center fadeInAnimated">
+        <div class="mx-auto mt-4 text-white text-center">
+            <h1><strong>Merajut Memori Bersama Kami.</strong></h1>
+            <h5>Dengan fasilitas dan pelayanan yang anda dambakan</h5>
+        </div>
+    </div>
+    <div class="justify-content-center mx-5">
+        <hr class="leftRightAnimated">
+    </div>
+    <div class="row px-2">
+        <div class="px-5">
+            <div class="btn-group float-end">
+                <input type="number" class="form-control reg" placeholder="Adults" name="Adult" id="AdultNum" >
+                <input type="number" class="form-control reg" placeholder="Infants" name="Infants" id="infantNum">
             </div>
         </div>
-        <div class="px-5 mx-5">
-            <hr class="leftRightAnimated">
-        </div>
-
     </div>
 
-
-    <footer class="main-footer text-white px-5">
-        <!-- To the right -->
-        <div class="float-right d-none d-sm-inline"></div>
-        <!-- Default to the left -->
-        &copy; 2023 PT.SAF Makmur Jaya, All Rights Reserved.
-    </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+    <div class="px-4 py-3 row">
+        <div class="col-3 ">
+            <div class="text-white">
+                Stasiun Asal
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="text-white">
+                Stasiun Tujuan
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="text-white">
+                Tanggal Keberangkatan
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="text-white">
+                Tanggal kembali
+            </div>
+        </div>
+       <div class="col-6">
+            <div class="btn-group container-fluid">
+                <select name="prodiDD" id="prodiDD" class="form-select reg">
+                    <option selected value="">piilh prodi</option>
+                    <option value="Informatika">Informatika</option>
+                    <option value="Teknik Industri">Teknik Industri</option>
+                    <option value="Sistem Informasi">Sistem Informasi</option>
+                </select>
+                <select name="prodiDD" id="prodiDD" class="form-select reg">
+                    <option selected value="">piilh prodi</option>
+                    <option value="Informatika">Informatika</option>
+                    <option value="Teknik Industri">Teknik Industri</option>
+                    <option value="Sistem Informasi">Sistem Informasi</option>
+                </select>
+            </div>
+       </div>
+       <div class="col-6">
+        <div class="btn-group container-fluid">
+            <input name="prodiDD" id="prodiDD" class="form-control reg" type="date"/>
+            <input name="prodiDD" id="prodiDD" class="form-control reg" type="date"/>
+        </div>
+   </div>
+   <div class="row px-5 my-3 container-fluid">
+    <button class="btn btn-primary ms-auto" style="width: 15%;">
+        Cari
+    </button>
+</div>
+@endsection
